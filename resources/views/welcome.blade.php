8bed808b2122f397aff2b713d34d4b9d3f3776d2
dilsonalfonso <dilson.05.09.da@gmail.com>
2025-02-01 22:38:59 -0500

    Todo configurado para la conexión a MySql en widows desde linux

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -167,6 +167,7 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        <p>Create by Ing. Dilson Manzano</p>
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
                 </div>
